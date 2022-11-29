5c2c823b2db1d8e6915fb7eb0f7e8bd471107096
Fábio M. Costa <fmc@ufg.br>
2022-11-29 11:32:27 -0300

    Minor fix on led_client

--- ClientCode/python/led_client.py
+++ ClientCode/python/led_client.py
@@ -13,7 +13,7 @@ from const import *
 def run():
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub (channel)
-        response = stub.BlinkLed(iot_service_pb2.LedRequest(state=int(sys.argv[1])),ledname=sys.argv[2])
+        response = stub.BlinkLed(iot_service_pb2.LedRequest(state=int(sys.argv[1]),ledname=sys.argv[2]))
 
     if response.ledstate == 1:
         print("Led state is on")
