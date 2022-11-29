6aed3e79f5e08668b5dfd8a3fbfc9d5aa0bf052a
Fábio M. Costa <fmc@ufg.br>
2022-11-29 11:19:13 -0300

    Added const.py to Client code

--- ClientCode/python/led_client.py
+++ ClientCode/python/led_client.py
@@ -7,9 +7,11 @@ import grpc
 import iot_service_pb2
 import iot_service_pb2_grpc
 
+from const import *
+
 
 def run():
-    with grpc.insecure_channel('34.136.25.200:50051') as channel:
+    with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub (channel)
         response = stub.BlinkLed(iot_service_pb2.LedMessage(state=int(sys.argv[1])))
 
